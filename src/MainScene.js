15b571e304a3ed5148f0abb97193474fa2d2539f
Yuliya Kandratsyeva <ykandratsyeva@gmail.com>
2021-01-22 15:15:30 -0500

    feat: added Effects for Path and started configuring camera

--- src/MainScene.js
+++ src/MainScene.js
@@ -1,14 +1,33 @@
 import React from 'react'
+import * as THREE from 'three'
 import { Canvas } from 'react-three-fiber'
 import { Stars, OrbitControls } from 'drei'
 import Track from './3d/Track'
+import Effects from './3d/Effects'
+import useStore from './store'
 
 export default function MainScene() {
+
+  const { fov } = useStore(state => state.mutation)
+  const actions = useStore(state => state.actions)
+
   return (
-    <Canvas>
-      <ambientLight intensity={0.5} />
+    <Canvas
+      concurrent
+      gl={{ antialias: false }}
+      // this position is somewhere near the middle 
+      camera={{ position: [0, 0, 200], near: 0.01, far: 10000, fov }}
+      onCreated={({ gl, camera }) => {
+        actions.init(camera)
+        gl.gammaInput = true
+        gl.toneMapping = THREE.LinearToneMapping
+        gl.setClearColor(new THREE.Color('#050209'))
+      }}>
+      <fog attach='fog' args={['#250736', 100, 700]}/>
+      <ambientLight intensity={0.25} />
       <Stars/>
       <Track/>
+      <Effects/>
       <OrbitControls/>
     </Canvas>
   )
--- src/store.js
+++ src/store.js
@@ -1,7 +1,7 @@
 import * as THREE from 'three'
-// state management library
 import create from 'zustand'
 import { Curves } from 'three/examples/jsm/curves/CurveExtras'
+import { addEffect } from 'react-three-fiber'
 
 const [useStore, api] = create((set, get) => {
 
@@ -9,11 +9,42 @@ const [useStore, api] = create((set, get) => {
   let track = new THREE.TubeBufferGeometry(spline, 250, 0.2, 10, true)
 
   return {
+
+    camera: undefined,
+
     mutation: {
       t: 0,
       position: new THREE.Vector3(),
       track,
-      scale: 15
+      scale: 15,
+      looptime: 40 *1000,
+      startTime: Date.now(),
+      binormal: new THREE.Vector3(),
+      normal: new THREE.Vector3(),
+      clock: new THREE.Clock(false),
+    },
+
+    actions: {
+      init(camera){
+        const { mutation, actions } = get()
+
+        set({ camera })
+        mutation.clock.start()
+
+        addEffect(() => {
+          const time = Date.now()
+          const t = (mutation.t = ((time - mutation.startTime) % mutation.looptime) / mutation.looptime)
+          mutation.position = track.parameters.path.getPointAt(t)
+          mutation.position.multiplyScalar(mutation.scale)
+
+          let warping = false
+          if (t > 0.3 && t < 0.4) {
+            if (!warping) {
+              warping = true
+            }
+          } else if (t > 0.5) warping = false
+        })
+      }
     }
   }
 })
